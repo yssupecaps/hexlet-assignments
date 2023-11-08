5f36472a12a21e4e8ac70e9790418ed31497e328
@hexlet/cli <support@hexlet.io>
2023-11-08 10:46:24 +0300

    submit java-collections-ru/lists

---
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -0,0 +1,31 @@
+package exercise;
+
+
+import org.apache.commons.collections4.CollectionUtils;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// BEGIN
+public final class App {
+
+    public static boolean scrabble(String someVars, String text) {
+        List<String> arrVars = new ArrayList<>(Arrays.asList(someVars.toLowerCase().split("")));
+        List<String> expect = new ArrayList<>(Arrays.asList(text.toLowerCase().split("")));
+        List<String> result = new ArrayList<>(App.intersection(expect,arrVars));
+        return String.join("",result).equalsIgnoreCase(text);
+    }
+
+
+    public static <T> List<T> intersection(List<T> list1, List<T> list2){
+
+        List<T> list = new ArrayList<T>();
+        for(T t : list1){
+            if( list2.contains(t)){
+                list.add(t);
+            }
+        }
+        return list;
+    }
+}
+//END
