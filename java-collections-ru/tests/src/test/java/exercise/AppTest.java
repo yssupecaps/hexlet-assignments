06edfd134d4430e9d90f8e0d5ae17459fdfe8b45
@hexlet/cli <support@hexlet.io>
2024-01-24 21:05:55 +0300

    submit java-collections-ru/tests

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -12,9 +12,18 @@ class AppTest {
     void testTake() {
         // BEGIN
         List<Integer> numbers1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-        var actual1 = App.take(numbers1, 2);
-        var actual2 = List.of(1,2);
-        assertThat(actual1).isEqualTo(actual2);
+
+        var actual1 = App.take(numbers1, 4);
+        var expected1 = List.of(1,2,3,4);
+        assertThat(actual1).isEqualTo(expected1);
+
+        var result2 = App.take(numbers1, -1);
+        var expected2 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        assertThat(result2).isNotEqualTo(expected2);
+
+        var result3 = App.take(numbers1, 2);
+        var expected3 = new ArrayList<>(Arrays.asList(1, 2));
+         assertThat(result3).isEqualTo(expected3);
         // END
     }
 }
